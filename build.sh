d92b92542ac7a07b8dce0bb8e057934bb670460b
bleedingfight <bleedingfight@126.com>
2021-09-11 11:41:12 +0800

    update:ssh config

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-local_path='local'
-if [ ! -d ${local_path} ] ;then 
-  mkdir ${local_path}
-fi 
+local_path='config/local'
+workspace=./workspace 
+function mkdir_safe(){
+	work_path=$1
+	if [ ! -d ${work_path} ];then
+		mkdir ${work_path}
+	fi 
+}
+mkdir_safe ${local_path}
+mkdir_safe ${workspace}
+
 cp ${HOME}/.ssh/id_rsa.pub ${local_path} 
-docker-compose up
+docker-compose up --build
